054aa40306d404a5cbce0d80cf5889ea24c78fa3
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-11-15 10:48:15 +0900

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header
+      class="flex items-center justify-between h-20 max-w-6xl mx-auto px-5 lg:px-0"
+    >
+      <div
+        class="border-2 bg-gradient-to-r from-sky-500 to-indigo-500 w-14 h-14 rounded-full"
+      ></div>
+      <ul class="md:flex items-center space-x-3 hidden">
+        <template :key="`navItem-$[i]`" v-for="(n, i) in nav">
+          <li>
+            <NuxtLink
+              class="inline-block px-3 py-2 bg-gray-50 rounded-full text-xl hover:bg-slate-200 transition-all duration-300"
+              :to="n.kink"
+            >
+              {{ n.name }}
+            </NuxtLink>
+          </li>
+        </template>
+      </ul>
+      <NuxtLink
+        to="#"
+        class="px-5 py-2 text-xl bg-violet-700 text-white rounded-full hover:bg-violet-900 transition-all duration-300"
+        >Download App</NuxtLink
+      >
+    </header>
+  </div>
+</template>
+
+<script setup>
+const nav = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/#",
+  },
+  {
+    name: "Product",
+    link: "/#",
+  },
+  {
+    name: "Contact",
+    link: "/#",
+  },
+];
+</script>
---
+++ components/Hero.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="mx-auto lg:max-w-6xl px-5 lg:px-0 pt-14 lg:pt-12">
+    <h1
+      class="capitalize font-semibold text-[38px] lg:text-7xl text-center leading-snug"
+    >
+      Welcome to the new<br /><span
+        class="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-violet-600 via-pink-600 to-amber-600"
+        >Frontend Development</span
+      >
+    </h1>
+    <p class="mt-10 text-2xl lg:text-3xl text-center font-medium">
+      This is my first time for using Vue.js, Nuxt.js and Tailwind CSS together.
+    </p>
+    <div class="mt-10 lg:mt-14 flex items-center justify-center space-x-4">
+      <NuxtLink
+        to="#"
+        class="px-6 py-3 text-xl bg-violet-700 text-white rounded-full hover:bg-violet-900 transition-all duration-300"
+        >Download App</NuxtLink
+      >
+      <NuxtLink
+        class="group text-xl overflow-hidden relative bg-gray-50 ring-1 hover:ring-transparent ring-gray-100 rounded-full transition-all duration-500"
+        to="#"
+      >
+        <div
+          class="absolute h-full w-0 group-hover:w-full transition-all duration-500 bg-violet-700"
+        ></div>
+        <span
+          class="block px-6 py-3 transition-all duration-500 group-hover:text-white relative"
+          >Take Tour</span
+        >
+      </NuxtLink>
+    </div>
+  </main>
+</template>
